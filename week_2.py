307acb2afab88f9188252dd7163a7a6ebe800b10
mjsekol <michael.sekol@mahoningctc.com>
2026-09-15 10:01:32 -0400

    Initial Commit - Week 2 of Class

---
+++ week_2.py
@@ -0,0 +1,34 @@
+# This is a comment
+# This is another comment
+"""
+This is docstring
+It is a multi line comment
+"""
+# Variables & Data Types 
+# Int (whole numbers), Float (decimal numbers), String (text), Boolean (True/False)
+# List (ordered collection of items), Tuple (ordered collection of items that cannot be changed), Set (unordered collection of unique items), Dictionary (collection of key-value pairs)
+
+x = 10 #? This is an integer aka int
+print(x) #? This will print the value of x
+print(x+5) #? This will print the value of x + 5
+
+your_temp = 98.6 #? This is a float
+print(your_temp) #? This will print the value of your_temp
+
+your_name = "John" #? This is a string
+print(your_name) #? This will print the value of your_name
+
+light_on = True #? This is a boolean
+print(light_on) #? This will print the value of light_on
+
+ninja_turtles_foods = ["Pizza", "Tofu", "Sushi", "Ramen", "Fried Rice", "Peas", "Hibachi"] #? This is a list
+print(ninja_turtles_foods) #? This will print the value of ninja_turtles_foods
+
+ninja_turtle_tuple = ("Leonardo", "Michelangelo", "Donatello", "Raphael") #? This is a tuple
+print(ninja_turtle_tuple) #? This will print the value of ninja_turtle_tuple
+
+ninja_turtle_skills = {"Leonardo": "Katana", "Michelangelo": "Nunchucks", "Donatello": "Bo Staff", "Raphael": "Sais"} #? This is a dictionary
+print(ninja_turtle_skills) #? This will print the value of ninja_turtle_skills
+
+  git config --global user.email "michael.sekol@mahoningctc.com"
+  git config --global user.name "mjsekol"
